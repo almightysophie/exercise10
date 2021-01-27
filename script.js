2943a523df5621912add1e0c2ff2159a346b524e
almightysophie <77300753+almightysophie@users.noreply.github.com>
2021-01-27 16:05:23 -0500

    xxx

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("button").click(function(){
       $("#home").fadeToggle();
-      $("#home").fadeToggle("slow");
-      $("#home").fadeToggle(3000);
+      $("#minbox").fadeToggle("slow");
+      $("#maxbox").fadeToggle(3000);
     });
   });
